6b81afcf98eec0dd2f389aac3c5e0a1bcd7a8b7c
ปิยชัย แก้วชุ่ม <kaonaka@hotmail.co.th>
2021-04-05 12:44:00 +0700

    add start and end

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <div>
-    <!-- display timer -->
-    <div>
-      <timer v-bind:start-app="startApp" />
+    <!-- When in naming State -->
+    <div v-if="state === 'nameState'">
+        <div class="learn-more">
+          <button @click="startState(); " class="learn-more">start</button>
+        </div>
     </div>
-    <!-- display Real text -->
-    <div v-bind:style="styleObject">
-      <div id="para" style="display: inline; top: 0px; width: 100% ;  "></div>
+
+    <!-- When in running State -->
+    <div v-if="state === 'runState'"> 
+        <div>
+            <timer @takeEnd="takeEnd" v-bind:start-app="startApp" />
+        </div>
+        <!-- display Real text -->
+        <div v-bind:style="styleObject">
+          
+            <div id="para" style="display: inline; top: 0px; width: 100% ;  "></div>
+        </div>
+        <!-- display Temp text -->
+        <div style="text-align: center; position:relative" >
+            <div v-if="!isPlay">
+              <p id="plus" style="display: inline ; top: 0px; width: 100%"></p>
+            </div>
+            <div id="tempText" style="display: inline ; top: 0px; width: 100%"></div> 
+        </div>
+        
+        
+        
+        <div v-if="isRecord && !isPlay" style="position: relative; margin: 0 auto; text-align: center">
+            <img class = "image-style" src="https://www.flaticon.com/svg/vstatic/svg/945/945185.svg?token=exp=1617564095~hmac=14fe0a401d86eaa536b3213040afa148" >
+        </div>
     </div>
-    <!-- display Temp text -->
-    <div  style="text-align: center;" >
-      <div id="tempText" style="display: inline ; top: 0px; width: 100%"></div> 
+
+    <!-- When in end State -->
+    <div v-if="state ==='endState'" >
+        <p class="popout">
+          <span>E</span><span>N</span><span>D</span><span>I</span><span>N</span><span>G</span>
+        </p>
     </div>
-    
-    <div v-if="!isPlay">
-      <p id="plus"></p>
-    </div>
+
 
   </div>
 </template>
@@ -32,6 +55,7 @@ export default {
 
   data() {
     return {
+      state: "nameState",
       myTrack: new Audio(),
       letter: json.letters,     //Import json file
       i: 0,                     //This integer for what number of alphabet in word              
@@ -41,13 +65,15 @@ export default {
       coloredString: [],        //this is a color string of word
       isStop: false,            //this boolean will turn to true when finish render 1 word
       isPlay: false,            //this boolean will turn to false when user press keyboard   
+      isRecord: false,
+      isEnd: false,
       arrayPool: [],            //Use to collect rendered word
       startApp:false,           //Boolean for sent data back to timer
       currentString: "",
       //Text style 
       fontSize:400,             //text size
       styleObject: {
-        top: '200px',
+        top: '50px',
         color: 'blue',
         position: 'fixed',
         left: '10px', 
@@ -56,6 +82,10 @@ export default {
   },
 
   methods: {
+    takeEnd(value){
+        this.isEnd = value;
+        this.state = "endState"
+    },
     //To get random number from word pool
     random(min,max,arrayPool) {
     var i = Math.floor(Math.random()*(max-min))+min;
@@ -136,7 +166,7 @@ export default {
       for (let index = 0; index < invisString.length; index++) {    //makeing array of html text 
         
           let lastInvis1 =
-            "<strong  style='font-size:"+(this.percent(this.fontSize , 100.5 )) +"px; opacity:2% ; font-weight: 401 !important; color: " +
+            "<strong  style='font-size:"+(this.percent(this.fontSize , 100.5 )) +"px; opacity:0% ; font-weight: 401 !important; color: " +
             this.coloredString[index] +
             ";'>" +
             invisString[index] +
@@ -249,44 +279,91 @@ export default {
       thisstring.splice(thisstring.length - num, 0, character);
       return thisstring;
     },
-
-    
+    runningFunction(){
+      if(!this.startApp) this.startApp = true //To return that game start
+            if(this.isRecord === true){
+              this.isRecord = false;
+            }
+            if(!this.isPlay){
+              var para = document.getElementById("plus");
+              var para2 = document.getElementById("para");
+              var para3 = document.getElementById("tempText");
+              this.thisString = []  
+              this.coloredString= [],
+              para.innerHTML = "<p style='font-size:"+ (this.percent(this.fontSize, 120))+"px ;color:black ;'>+</p>";
+              para2.innerHTML = "";
+              para3.innerHTML = "";
+              this.j= this.random(0,this.letter.length,this.arrayPool)
+              this.previusJ = this.j
+            setTimeout(()=>{
+            if (!this.isPlay) {
+                this.makeTempString()
+                this.playSound();
+              }
+            },2000)
+            }
+    },
+    turnRecord(){
+      if(!this.isPlay && this.isRecord === false){
+            this.isRecord = true;
+        }
+    },
+    startState(){
+      this.state = 'runState' ; 
+      setTimeout(()=>{
+            this.turnRecord() ; 
+            this.runningFunction();
+            },100)
+      
+    }
 
   },
   mounted: function() {
-    //add event when user interact with keyboard
-      window.addEventListener("keyup", (event) => {
-        console.log(event.code)
-        if(event.code === 'KeyR'){
-          if(!this.isPlay){      
-            this.j = this.previusJ
-            this.playSound();
-          }
-        }
+
+    //When app in name state
+    window.addEventListener("keypress", (event) => {
+      console.log(this.state)
+      if(this.state === 'nameState'){
+
+      }
+      else if(this.state === 'runState'){
         if(event.code === 'Space'){
-          if(!this.startApp) this.startApp = true //To return that game start
-          if(!this.isPlay){
-            var para = document.getElementById("plus");
-            var para2 = document.getElementById("para");
-            var para3 = document.getElementById("tempText");
-            this.thisString = []  
-            this.coloredString= [],
-            para.innerHTML = "<p style='font-size:"+ (this.percent(this.fontSize, 150))+"px ;color:black ;  text-align: center '>+</p>";
-            para2.innerHTML = "";
-            para3.innerHTML = "";
-            this.j= this.random(0,this.letter.length,this.arrayPool)
-            this.previusJ = this.j
-          setTimeout(()=>{
-          if (!this.isPlay) {
-              this.makeTempString()
-              this.playSound();
-              
-            }
-          },2000)
-          }
-        }
-        
+          this.turnRecord()
+        } 
+      }
+      else if(this.state === 'endState'){
+
+      }
+
     });
+    
+      //add event when user interact with keyboard
+    window.addEventListener("keyup", (event) => {
+      console.log(event.code)
+      //When in nameState
+      if(this.state === 'nameState'){
+
+      }
+      //When in runningState
+      else if(this.state === 'runState' ){     
+                        
+          if(event.code === 'KeyR'){
+            if(!this.isPlay){      
+              this.j = this.previusJ
+              this.playSound();
+            }
+          }
+          if(event.code === 'Space' ){
+            this.runningFunction();
+          }
+      }
+      //When in endState
+      else if(this.state === 'endState'){
+
+      }
+    });
+    
+    
   },
 };
 </script>
@@ -294,27 +371,20 @@ export default {
 <style >
 @import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
 .image_style{
-  position: relative;
-  z-index: -900;
-  width: 5%;
+  position: fixed;
+  width: 20%;
   display: block;
   margin-left: auto;
   margin-right: auto;
+  color: rebeccapurple;
   
 }
 
 * {
   padding: 0;
   margin: 0;
-  box-sizing: border-box;
-  font-family: 'Sarabun', sans-serif;
-}
-
-div.text_position{
-  position:static;
-  text-align: center;
-  font-size: 400px;
   
+  font-family: 'Sarabun', sans-serif;
 }
 
 .center {
@@ -325,12 +395,130 @@ div.text_position{
   border: 3px solid green; 
 }
 
-strong{
-  
+.image-style{
+  position: fixed;
+  top: 85%;
+  left: 50%;
+  width: 10%;
+  height: auto;
+  transform: translate(-50%, -50%);
 }
 
 b{
   font-weight: 399 !important;
+}
+
+/* For animation button */
+@import url('https://fonts.googleapis.com/css?family=Rubik:700&display=swap');
+div.learn-more {
+	 font-family: 'Rubik', sans-serif;
+	 font-size: 1rem;
+	 line-height: 1.5;
+	 display: flex;
+	 align-items: center;
+	 justify-content: center;
+	 margin: 0;
+	 min-height: 100vh;
+	 background: #fff;
+}
+ button {
+	 position: relative;
+	 display: inline-block;
+	 cursor: pointer;
+	 outline: none;
+	 border: 0;
+	 vertical-align: middle;
+	 text-decoration: none;
+	 font-size: inherit;
+	 font-family: inherit;
+}
+ button.learn-more {
+   font-size: 100px;
+	 font-weight: 600;
+	 color: #382b22;
+	 text-transform: uppercase;
+	 padding: 1.25em 2em;
+	 background: #fff0f0;
+	 border: 2px solid #b18597;
+	 border-radius: 0.75em;
+	 transform-style: preserve-3d;
+	 transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), background 150ms cubic-bezier(0, 0, 0.58, 1);
+}
+ button.learn-more::before {
+	 position: absolute;
+	 content: '';
+	 width: 100%;
+	 height: 100%;
+	 top: 0;
+	 left: 0;
+	 right: 0;
+	 bottom: 0;
+	 background: #f9c4d2;
+	 border-radius: inherit;
+	 box-shadow: 0 0 0 2px #b18597, 0 0.625em 0 0 #ffe3e2;
+	 transform: translate3d(0, 0.75em, -1em);
+	 transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
+}
+ button.learn-more:hover {
+	 background: #ffe9e9;
+	 transform: translate(0, 0.25em);
+}
+ button.learn-more:hover::before {
+	 box-shadow: 0 0 0 2px #b18597, 0 0.5em 0 0 #ffe3e2;
+	 transform: translate3d(0, 0.5em, -1em);
+}
+ button.learn-more:active {
+	 background: #ffe9e9;
+	 transform: translate(0em, 0.75em);
+}
+ button.learn-more:active::before {
+	 box-shadow: 0 0 0 2px #b18597, 0 0 #ffe3e2;
+	 transform: translate3d(0, 0, -1em);
+}
+
+/* for animation ending */
+.popout {
+  text-align: center;
+	 font-family: Futura, sans-serif;
+	 font-weight: 900;
+	 font-size: 350px;
+	 padding: 80px;
+}
+ @keyframes ani {
+	 0% {
+		 transform: translate3d(0, 0, 0);
+		 text-shadow: 0em 0em 0 lightblue;
+		 color: black;
+	}
+	 30% {
+		 transform: translate3d(0, 0, 0);
+		 text-shadow: 0em 0em 0 lightblue;
+		 color: black;
+	}
+	 70% {
+		 transform: translate3d(0.08em, -0.08em, 0);
+		 text-shadow: -0.08em 0.08em lightblue;
+		 color: black;
+	}
+	 100% {
+		 transform: translate3d(0.08em, -0.08em, 0);
+		 text-shadow: -0.08em 0.08em lightblue;
+		 color: black;
+	}
+}
+ .popout span {
+	 position: relative;
+	 display: inline-block;
+	 animation: ani 1s infinite alternate cubic-bezier(0.86, 0, 0.07, 1);
+}
+ .popout span:nth-last-child(1n) {
+	 animation-delay: -0.1666666667s;
+}
+ .popout span:nth-last-child(2n) {
+	 animation-delay: -0.3333333333s;
+}
+ .popout span:nth-last-child(3n) {
+	 animation-delay: -0.5s;
 }
 
 </style>
--- src/components/layout/timer.vue
+++ src/components/layout/timer.vue
@@ -2,7 +2,7 @@
   <div id="timer">
     <span style="position: absolute;
     bottom: 0;
-    left: 0;">time {{displayMinute}}:{{displaySecond}}</span>
+    left: 0;">Time {{displayMinute}}:{{displaySecond}}</span>
   </div>
 </template>
 
@@ -14,163 +14,41 @@ export default {
   },
   data() {
     return {
-      Minute: 2,
-      Second: 0,
-      displayMinute: "",
-      displaySecond: "",
+      Minute: 0,
+      Second: 15,
+      displayMinute: "00",
+      displaySecond: "00",
       test:true,
+      isEnd: false,
+      all:5,
     };
   },
   methods: {},
   mounted() {
     setInterval(() => {
-      if(this.startApp){
-        this.Second--
-        if(this.Second < 0){
-          this.Minute--
-          this.Second = 59
-
-        }
-        
-        if(this.Second < 10){
-          this.displaySecond = "0"+this.Second
+      if(this.startApp ){
+        if(this.all > 0){
+          this.all-=1
+          this.Second = this.all%60
+          this.Minute = parseInt(this.all/60)
+          if(this.Second < 10){
+            this.displaySecond = "0"+this.Second
+          }
+          else{
+            this.displayMinute = this.Minute
+            this.displaySecond = this.Second
+          }
         }
         else{
-          this.displayMinute = this.Minute
-          this.displaySecond = this.Second
+          this.isEnd = true;
+          this.$emit("takeEnd",this.isEnd)
+          console.log("i sas")
         }
+
+        
+
       }
     }, 1000);
-    // Create Countdown
-    // var Countdown = {
-    //   // Backbone-like structure
-    //   $el: document.getElementsByClassName('.countdown')[0],
-
-    //   // Params
-    //   countdown_interval: null,
-    //   total_seconds: 0,
-
-    //   // Initialize the countdown
-    //   init: function() {
-    //     // DOM
-    //     this.$ = {
-    //       hours: this.$el.find(".bloc-time.hours .figure"),
-    //       minutes: this.$el.find(".bloc-time.min .figure"),
-    //       seconds: this.$el.find(".bloc-time.sec .figure"),
-    //     };
-
-    //     // Init countdown values
-    //     this.values = {
-    //       hours: this.$.hours.parent().attr("data-init-value"),
-    //       minutes: this.$.minutes.parent().attr("data-init-value"),
-    //       seconds: this.$.seconds.parent().attr("data-init-value"),
-    //     };
-
-    //     // Initialize total seconds
-    //     this.total_seconds =
-    //       this.values.hours * 60 * 60 +
-    //       this.values.minutes * 60 +
-    //       this.values.seconds;
-
-    //     // Animate countdown to the end
-    //     this.count();
-    //   },
-
-    //   count: function() {
-    //     var that = this,
-    //       $hour_1 = this.$.hours.eq(0),
-    //       $hour_2 = this.$.hours.eq(1),
-    //       $min_1 = this.$.minutes.eq(0),
-    //       $min_2 = this.$.minutes.eq(1),
-    //       $sec_1 = this.$.seconds.eq(0),
-    //       $sec_2 = this.$.seconds.eq(1);
-
-    //     this.countdown_interval = setInterval(function() {
-    //       if (that.total_seconds > 0) {
-    //         --that.values.seconds;
-
-    //         if (that.values.minutes >= 0 && that.values.seconds < 0) {
-    //           that.values.seconds = 59;
-    //           --that.values.minutes;
-    //         }
-
-    //         if (that.values.hours >= 0 && that.values.minutes < 0) {
-    //           that.values.minutes = 59;
-    //           --that.values.hours;
-    //         }
-
-    //         // Update DOM values
-    //         // Hours
-    //         that.checkHour(that.values.hours, $hour_1, $hour_2);
-
-    //         // Minutes
-    //         that.checkHour(that.values.minutes, $min_1, $min_2);
-
-    //         // Seconds
-    //         that.checkHour(that.values.seconds, $sec_1, $sec_2);
-
-    //         --that.total_seconds;
-    //       } else {
-    //         clearInterval(that.countdown_interval);
-    //       }
-    //     }, 1000);
-    //   },
-
-    //   animateFigure: function($el, value) {
-    //     var that = this,
-    //       $top = $el.find(".top"),
-    //       $bottom = $el.find(".bottom"),
-    //       $back_top = $el.find(".top-back"),
-    //       $back_bottom = $el.find(".bottom-back");
-
-    //     // Before we begin, change the back value
-    //     $back_top.find("span").html(value);
-
-    //     // Also change the back bottom value
-    //     $back_bottom.find("span").html(value);
-
-    //     // Then animate
-    //     TweenMax.to($top, 0.8, {
-    //       rotationX: "-180deg",
-    //       transformPerspective: 300,
-    //       ease: Quart.easeOut,
-    //       onComplete: function() {
-    //         $top.html(value);
-
-    //         $bottom.html(value);
-
-    //         TweenMax.set($top, { rotationX: 0 });
-    //       },
-    //     });
-
-    //     TweenMax.to($back_top, 0.8, {
-    //       rotationX: 0,
-    //       transformPerspective: 300,
-    //       ease: Quart.easeOut,
-    //       clearProps: "all",
-    //     });
-    //   },
-
-    //   checkHour: function(value, $el_1, $el_2) {
-    //     var val_1 = value.toString().charAt(0),
-    //       val_2 = value.toString().charAt(1),
-    //       fig_1_value = $el_1.find(".top").html(),
-    //       fig_2_value = $el_2.find(".top").html();
-
-    //     if (value >= 10) {
-    //       // Animate only if the figure has changed
-    //       if (fig_1_value !== val_1) this.animateFigure($el_1, val_1);
-    //       if (fig_2_value !== val_2) this.animateFigure($el_2, val_2);
-    //     } else {
-    //       // If we are under 10, replace first figure with 0
-    //       if (fig_1_value !== "0") this.animateFigure($el_1, 0);
-    //       if (fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-    //     }
-    //   },
-    // };
-
-    // // Let's go !
-    // Countdown.init();
   },
 };
 </script>
